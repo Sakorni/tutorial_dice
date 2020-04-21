9d169fdfc33c7a467e3ff51025e1e9422303214f
Sakorni <ssakorni@gmail.com>
2020-04-21 22:32:27 +0300

    finished the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+String d1 = 'images/dice1.png';
+String d2 = 'images/dice2.png';
+String d3 = 'images/dice3.png';
+String d4 = 'images/dice4.png';
+String d5 = 'images/dice5.png';
+String d6 = 'images/dice6.png';
+List<String> dices = [d1, d2, d3, d4, d5, d6];
+int first = 0;
+int second = 0;
+Random random = new Random();
 void main() {
   return runApp(
     MaterialApp(
@@ -15,9 +27,39 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  void reset() => setState(() {
+        first = random.nextInt(6);
+        second = random.nextInt(6);
+      });
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: reset,
+              child: Image.asset(
+                dices[first],
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: reset,
+              child: Image.asset(
+                dices[second],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
